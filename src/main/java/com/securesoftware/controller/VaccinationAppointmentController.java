6f81483e005b01f924a2931a8d545e1f13cb4e71
brianbyrne99 <brian.byrne3@ucdconnect.ie>
2022-03-09 13:26:57 +0000

    Real time dates included

--- src/main/java/com/securesoftware/controller/VaccinationAppointmentController.java
+++ src/main/java/com/securesoftware/controller/VaccinationAppointmentController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -79,88 +81,65 @@ public class VaccinationAppointmentController {
                 "19:45"
         };
 
-        String[] dates = {
-                "00/03/2021",
-                "01/03/2021",
-                "02/03/2021",
-                "03/03/2021",
-                "04/03/2021",
-                "05/03/2021",
-                "06/03/2021",
-                "07/03/2021",
-                "08/03/2021",
-                "09/03/2021",
-                "10/03/2021",
-                "11/03/2021",
-                "12/03/2021",
-                "13/03/2021",
-                "14/03/2021",
-                "15/03/2021",
-                "16/03/2021",
-                "17/03/2021",
-                "18/03/2021",
-                "19/03/2021",
-                "20/03/2021",
-                "21/03/2021",
-                "22/03/2021",
-                "23/03/2021",
-                "24/03/2021",
-                "25/03/2021",
-                "26/03/2021",
-                "27/03/2021",
-                "28/03/2021",
-                "29/03/2021",
-                "30/03/2021",
-                "31/03/2021"
-        };
+        String[] dates = dateCalculator();
 
         String[] vaccinationCentres = {
-            "City West",
-            "Aviva Stadium",
-            "Dalkey Boat Club",
-            "Lusk Tennis Club",
-            "Walkinstown",
-            "Mater Private",
-            "Blanchardstown Hospital"
+                "City West",
+                "Aviva Stadium",
+                "Dalkey Boat Club",
+                "Lusk Tennis Club",
+                "Walkinstown",
+                "Mater Private",
+                "Blanchardstown Hospital"
         };
 
         String[] vaxBrands = {
-            "Pfizer",
-            "Moderna"
+                "Pfizer",
+                "Moderna"
         };
 
         // For loop to generate times
- 
+
         ArrayList<String> formattedTimes = new ArrayList<>();
 
-        for (int i=0;i < dates.length; i++){
-            for (int j = 0;j <times.length;j++){
-                String tempTime = times[j]+" "+dates[i];
+        for (int i = 0; i < dates.length; i++) {
+            for (int j = 0; j < times.length; j++) {
+                String tempTime = times[j] + " " + dates[i];
                 formattedTimes.add(tempTime);
             }
         }
 
-      // For loop to assign a slot to each hospital
-      for (int i = 0;i < vaccinationCentres.length; i++)  {
-          if (i%2 == 0){
-              for (String time : formattedTimes)
-              allSlots.add(new VaccinationSlot(vaxBrands[0], time, vaccinationCentres[i]));
-          }
+        // For loop to assign a slot to each hospital
+        for (int i = 0; i < vaccinationCentres.length; i++) {
+            if (i % 2 == 0) {
+                for (String time : formattedTimes)
+                    allSlots.add(new VaccinationSlot(vaxBrands[0], time, vaccinationCentres[i]));
+            }
 
-          if (i%2 == 1){
-            for (String time : formattedTimes)
-            allSlots.add(new VaccinationSlot(vaxBrands[1], time, vaccinationCentres[i]));
+            if (i % 2 == 1) {
+                for (String time : formattedTimes)
+                    allSlots.add(new VaccinationSlot(vaxBrands[1], time, vaccinationCentres[i]));
+            }
         }
-      }
 
-      return allSlots;
+        return allSlots;
 
+    }
+
+    // Returns a current list of dates from today to 30 days from now
+    public static String[] dateCalculator() {
+        String currentDate = java.time.LocalDate.now().toString();
+        String[] dates = new String[31];
+        for (int i = 0; i < 31; i++) {
+            dates[i] = java.time.LocalDate
+                    .parse(currentDate)
+                    .plusDays(i)
+                    .toString();
+        }
+        return dates;
     }
 
     // Small test to display how this is going to function
-    public static void main(String[] args) {
-        System.out.println("Length "+setSlots().size()+"\nFirstElement "+setSlots().get(0).getTimeSlot()+" "+setSlots().get(0).getVaccinationCentre()+" "+setSlots().get(0).getBrandType());
-        
-    }
+
 
 }
