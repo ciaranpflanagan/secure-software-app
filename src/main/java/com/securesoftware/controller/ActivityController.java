b0dff86cda9e87d4ac08df00acb82b88929a7531
brianbyrne99 <brian.byrne3@ucdconnect.ie>
2022-03-10 14:47:39 +0000

    checkpoint

--- src/main/java/com/securesoftware/controller/ActivityController.java
+++ src/main/java/com/securesoftware/controller/ActivityController.java
@@ -1,17 +1,22 @@
 package com.securesoftware.controller;
 
 import com.securesoftware.exception.UserNotFoundException;
+import com.securesoftware.model.User;
 import com.securesoftware.model.VaccinationAppointment;
 import com.securesoftware.model.VaccinationSlot;
 import com.securesoftware.repository.ActivityRepository;
+import com.securesoftware.repository.UserRepository;
 import com.securesoftware.repository.VaccinationAppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -21,8 +26,28 @@ import java.util.Map;
 public class ActivityController {
 
     @Autowired
-    ActivityRepository activityRepository;
+    ActivityRepository ActivityRepository;
 
-    
+    @Autowired
+    static UserRepository userRepository;
+
+    @RequestMapping(value = "/recent-activity", method = RequestMethod.GET)
+    public String getRecentActivity(Model model, Authentication authentication, HttpServletRequest request){
+        String userId = (String) authentication.getCredentials();
+        long temp = 4;
+        User user = userRepository.getById(temp);
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        //Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        //String email = "brianbyrne@gmail.com";
+        //String userId = (String) authentication.getCredentials();
+        long temp = 4;
+        //User user = userRepository.getById(temp);
+
+        System.out.println(temp);
+    }
     
 }
