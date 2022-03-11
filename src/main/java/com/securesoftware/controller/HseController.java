51b5c37f486312013d72287d5664c2bf669b7d9b
Ciaran Flanagan <ciaran@cicisoft.net>
2022-03-11 18:55:36 +0000

    Added frontend for editing appointment

--- src/main/java/com/securesoftware/controller/HseController.java
+++ src/main/java/com/securesoftware/controller/HseController.java
@@ -48,7 +48,71 @@ public class HseController {
         return "hse/appointments";
     }
 
-    @PostMapping("/edit-appointments")
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable(value = "id") Long appointmentId) {
+        String[] times = {
+                "09:00",
+                "09:15",
+                "09:30",
+                "09:45",
+                "10:00",
+                "10:15",
+                "10:30",
+                "10:45",
+                "11:00",
+                "11:15",
+                "11:30",
+                "11:45",
+                "12:00",
+                "12:15",
+                "12:30",
+                "12:45",
+                "13:00",
+                "13:15",
+                "13:30",
+                "13:45",
+                "14:00",
+                "14:15",
+                "14:30",
+                "14:45",
+                "15:00",
+                "15:15",
+                "15:30",
+                "15:45",
+                "16:00",
+                "16:15",
+                "16:30",
+                "16:45",
+                "17:00"
+        };
+        model.addAttribute("times", times);
+
+        String[] dates = VaccinationAppointmentController.dateCalculator();
+        model.addAttribute("dates", dates);
+
+        String[] centres = {
+                "City West",
+                "Aviva Stadium",
+                "Lusk Tennis Club",
+                "Walkinstown",
+                "Mater Private",
+                "Blanchardstown Hospital"
+        };
+        model.addAttribute("centres", centres);
+        
+        String[] brands = {
+                "Pfizer",
+                "Moderna"
+        };
+        model.addAttribute("brands", brands);
+
+        VaccinationAppointment appointment = vaccinationAppointmentRepository.findById(appointmentId).get();
+        model.addAttribute("appointment", appointment);
+
+        return "hse/edit-appointment";
+    }
+
+    @PostMapping("/edit-appointment")
     public String editAppointments(@RequestParam Map<String, String> allParams, Model model){
         String idAsString = "0"; // Note this will be a parameter
         int id = Integer.parseInt(idAsString);
