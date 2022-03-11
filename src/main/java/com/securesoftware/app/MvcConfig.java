e8f2a0a0c847cd592147a5ecb0541ac91dcd2b2a
Ciaran Flanagan <ciaran@cicisoft.net>
2022-03-11 10:40:57 +0000

    Updated frontend views inline with improved frontend

--- src/main/java/com/securesoftware/app/MvcConfig.java
+++ src/main/java/com/securesoftware/app/MvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/home").setViewName("home");
-		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/").setViewName("hello");
 		registry.addViewController("/hello").setViewName("hello");
 		registry.addViewController("/login").setViewName("login");
 	}
