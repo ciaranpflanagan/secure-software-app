4e3418afeadf1fb3df300414322c80f00146e4f2
Ciaran Flanagan <ciaran@cicisoft.net>
2022-03-11 20:25:23 +0000

    Created stats controller

---
+++ src/main/java/com/securesoftware/controller/StatsController.java
@@ -0,0 +1,23 @@
+package com.securesoftware.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/stats")
+public class StatsController {
+    /**
+     * Displays the forum
+     */
+    @GetMapping("/")
+    public String showForum(Model model) {
+        // List<Appoin> posts = forumRepository.findAll();
+        // model.addAttribute("posts", posts);
+
+        return "stats/aggregate-stats";
+    }
+}
