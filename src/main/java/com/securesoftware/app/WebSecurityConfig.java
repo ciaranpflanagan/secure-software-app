02663073025b0d094ec63b78ec88e3eb97a0ae53
Ciaran Flanagan <ciaran@cicisoft.net>
2022-05-11 19:22:51 +0100

    Fixed broken session management

--- src/main/java/com/securesoftware/app/WebSecurityConfig.java
+++ src/main/java/com/securesoftware/app/WebSecurityConfig.java
@@ -38,6 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 			.logout()
 				.permitAll();
+
+		http.sessionManagement().maximumSessions(1).maxSessionsPreventsLogin(false);
 	}
 
     @Override
